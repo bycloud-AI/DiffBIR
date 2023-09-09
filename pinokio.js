74888e8814bb0bcac0b3ef4762121e7862f34518
bycloudai <cloudbby200@gmail.com>
2023-09-08 21:01:57 -0400

    weights folder and .jsons

---
+++ pinokio.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: "Tokenflow",
+      description: "TokenFlow: Consistent Diffusion Features for Consistent Video Editing",
+      icon: "icon.png",
+  menu: [{
+    html: "<i class='fa-solid fa-plug'></i> Install",
+    type: "link",
+    href: "install.json?run=true&fullscreen=true"
+  },
+  {
+    html: "<i class='fa-solid fa-power-off'></i> Start",
+    type: "link",
+    href: "start.json?run=true&fullscreen=true"
+  },
+  {
+    html: "<i class='fa-solid fa-gear'></i> Config",
+    type: "link",
+    href: "config.json?run=true&fullscreen=true"
+  }]
+}
